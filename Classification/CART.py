0afdc6b51bee2720c495f6cce571e1bf5925d1c9
Shao-Fu-Wang <swangem@connect.ust.hk>
2022-03-27 17:43:22 +0800

    some tools

---
+++ Classification/CART.py
@@ -0,0 +1,20 @@
+import math
+
+err = 0.000000000000000000000000000000001
+
+while(1):
+    totalInfo = float(input("The Target split: "))
+    firstInfo = float(input("T_one: "))
+    secondInfo = float(input("T_two: "))
+    attrSplit = float(input("The attribute split(T_one_partion): "))
+    totalInfo = 1 - pow(totalInfo, 2) - pow(1-totalInfo, 2)
+    firstInfo = 1 - pow(firstInfo, 2) - pow(1-firstInfo, 2)
+    secondInfo = 1 - pow(secondInfo, 2) - pow(1-secondInfo, 2)
+    attrInfo = attrSplit*firstInfo + (1-attrSplit)*secondInfo
+    Cartgain = totalInfo - attrInfo
+    print()
+    print("firstInfo: ",  firstInfo)
+    print("secondInfo: ", secondInfo)
+    print("attrInfo: ", attrInfo)
+    print("CART gain: ", Cartgain)
+    print()
---
+++ Classification/ID3, C4.5.py
@@ -0,0 +1,24 @@
+import math
+
+err = 0.000000000000000000000000000000001
+
+while(1):
+    totalInfo = float(input("The Target split: "))
+    firstInfo = float(input("T_one: "))
+    secondInfo = float(input("T_two: "))
+    attrSplit = float(input("The attribute split(T_one_portion): "))
+    totalInfo = totalInfo*-math.log2(totalInfo+err)-(1-totalInfo)*math.log2(1-totalInfo+err)
+    firstInfo = firstInfo*-math.log2(firstInfo+err)-(1-firstInfo)*math.log2(1-firstInfo+err)
+    secondInfo = secondInfo*-math.log2(secondInfo+err)-(1-secondInfo)*math.log2(1-secondInfo+err)
+    attrInfo = attrSplit*firstInfo + (1-attrSplit)*secondInfo
+    splitInfo = attrSplit*-math.log2(attrSplit+err)-(1-attrSplit)*math.log2(1-attrSplit+err)
+    ID3gain = totalInfo - attrInfo
+    C45gain = ID3gain/splitInfo
+    print()
+    print("firstInfo: ",  firstInfo)
+    print("secondInfo: ", secondInfo)
+    print("attrInfo: ", attrInfo)
+    print("ID3 gain: ", ID3gain)
+    print("splitInfo: ", splitInfo)
+    print("C4.5 gain: ", C45gain)
+    print()
